c0e69747043558802e2670206ba0bb0fb9ce875c
Yael Moya <yael.moya.mac@outlook.com>
2023-12-07 14:04:13 -0600

    feat: Add schema name update user dto

--- src/main/java/mx/com/aey/user/infrastructure/rest/dto/UpdateUserDto.java
+++ src/main/java/mx/com/aey/user/infrastructure/rest/dto/UpdateUserDto.java
@@ -14,7 +14,7 @@ import java.util.UUID;
 @Builder
 @Getter
 @Setter
-@Schema(name = "User")
+@Schema(name = "UpdateUser")
 public class UpdateUserDto {
     @JsonProperty
     @Schema(readOnly = true)
